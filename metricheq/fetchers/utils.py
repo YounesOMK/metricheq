0d606d3f99f8ca2223d3bccfc2d9cb365a4d0cbe
Younes Oumakhou <younesoumakhousudo@gmail.com>
2023-12-21 14:46:44 +0100

    refactore: remove unused  code

--- metricheq/fetchers/utils.py
+++ metricheq/fetchers/utils.py
@@ -1,17 +1,7 @@
 from enum import Enum
 
-fetcher_registry = {}
-
 
 class DurationFormat(str, Enum):
     SECONDS = "seconds"
     MINUTES = "minutes"
     HOURS = "hours"
-
-
-def register_fetcher(file_name):
-    def decorator(fetcher_class):
-        fetcher_registry[fetcher_class.__name__] = file_name
-        return fetcher_class
-
-    return decorator
